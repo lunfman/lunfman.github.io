9bd514202498965b981ed24cdc1769c83f03b1ec
lunfman <smart275@gmail.com>
2022-06-12 17:10:01 +0300

    fix: HeroImage - fix fontsize problem on diffirent devices

--- portfolio/src/components/HeroImage.tsx
+++ portfolio/src/components/HeroImage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './hero-image.css'
 
 const HeroImageStyle = {
     width: '100vw',
@@ -17,13 +18,12 @@ const HeroTitleStyle = {
     fontFamily: 'Inter',
     fontStyle: 'italic',
     fontWeight: '800',
-    fontSize: '7rem'
 } as React.CSSProperties
 
 export default function HeroImage() {
     return(
         <div style={HeroImageStyle}>
-            <h1 style={HeroTitleStyle}><span style={{color:'pink'}}>L</span>unfmans's Portfolio</h1>
+            <h1 style={HeroTitleStyle} className='title'><span style={{color:'pink'}}>L</span>unfmans's Portfolio</h1>
         </div>
     )
 }
